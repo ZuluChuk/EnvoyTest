c4842dd2acbbb8da89398c8374909f511a3582c4
Stephen Rado <stejay@thirteen37.za.net>
2017-05-18 20:49:45 +0100

    added comment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//product page image gallery
 $( ".js-thumb" ).click(function() {
 	var imgSrc = $(this).find("img").attr("src")
   	$(this).addClass("active").parents().find(".js-main-image").attr("src", imgSrc);
