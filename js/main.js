3ba02a57faf851a83f08666caef59d0160069141
Stephen Rado <stejay@thirteen37.za.net>
2017-05-18 20:45:15 +0100

    added product page js and features

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$( ".js-thumb" ).click(function() {
+	var imgSrc = $(this).find("img").attr("src")
+  	$(this).addClass("active").parents().find(".js-main-image").attr("src", imgSrc);
+  	$(this).siblings().removeClass("active");	
+});
